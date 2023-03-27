8235e6213a5ddf2e0b355f1b7b9bb76a802f6f78
Arnaud Mazeyrat <kpn41@hotmail.fr>
2023-03-27 10:15:32 +0200

    photo et icones

---
+++ src/pages/Home.js
@@ -0,0 +1,8 @@
+//import datas from '../datas/datas.json';
+//import styles from "../style/modules_css/Home.module.css";
+
+function Home() {
+    return <p>ça marche !</p>
+}
+
+export default Home
